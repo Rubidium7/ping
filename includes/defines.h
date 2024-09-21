f6987a75f0d3dd57ed673df2235daad1c9ea03d2
Noel Lonka <noel.lonka@gmail.com>
2024-09-21 09:33:29 +0300

    feat(core): create header files

---
+++ includes/defines.h
@@ -0,0 +1,11 @@
+
+#ifndef DEFINES_H
+# define DEFINES_H
+
+typedef enum e_error
+{
+    ERROR_UNKNOWN_HOST = 1,
+    ERROR_ARG_COUNT = 64,
+}   t_error;
+
+#endif
--- includes/ping.h
+++ includes/ping.h
@@ -2,6 +2,10 @@
 #ifndef PING_H
 # define PING_H
 
+# include "defines.h"
+# include "structs.h"
+# include <netdb.h>
+# include <arpa/inet.h>
 # include <stdio.h>
 # include <unistd.h>
 
