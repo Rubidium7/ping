86a8e1da2fa4f8f4fd5c8e4e0c19b62430c6890a
Noel Lonka <noel.lonka@gmail.com>
2024-09-21 09:33:55 +0300

    feat(main): add basic dns lookup

--- main/main.c
+++ main/main.c
@@ -1,13 +1,42 @@
 
 #include "ping.h"
 
+// struct hostent {
+//                char  *h_name;            /* official name of host */
+//                char **h_aliases;         /* alias list */
+//                int    h_addrtype;        /* host address type */
+//                int    h_length;          /* length of address */
+//                char **h_addr_list;       /* list of addresses */
+//            }
+
+
+void	print_array(char **array)
+{
+	for (size_t i = 0; array[i]; i++)
+	{
+		printf("%s\n", array[i]);
+	}
+
+}
+
 int main(int ac, char **av)
 {
+	struct hostent *host;
+
 	if (ac < 2)
 	{
 		dprintf(STDERR_FILENO, "ping: missing host operand\n");	
-		return (64);
+		return (ERROR_ARG_COUNT);
 	}
+
+	host = gethostbyname(av[1]);
+	if (!host)
+	{
+		dprintf(STDERR_FILENO, "ping: unknown host\n");	
+		return (ERROR_UNKNOWN_HOST);
+	}
+	printf("%s\n", host->h_name);
+	printf("%s\n", inet_ntoa(*(struct in_addr *)host->h_addr_list[0]));
 
 	return (0);
 }
